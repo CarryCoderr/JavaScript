f770a095c185a236c22d8088ad31245990c4acde
Carry Coder <carrycoderr@gmail.com>
2023-05-01 19:35:17 +0530

    day1

---
+++ Tutorial/Day1/1.console.js
@@ -0,0 +1,25 @@
+console.log("Hello world");
+console.log(2 + 2);
+console.log("Hello this is new message");
+
+console.warn("This is a warring");
+
+console.error("This is a error");
+
+
+console.info("THis is a info");//use for debug purpose
+
+// console.clear();
+
+// comment 
+// single line comment
+//this is a message for comment purpose
+
+
+// multiline comment
+
+/*this is a multiline;
+comment*/;
+
+console.log("this is a new message");
+console.warn("this is a new warring");
