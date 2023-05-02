ac82a9a5242e9a3683adde4b76e66309f112b02d
Carry Coder <carrycoderr@gmail.com>
2023-05-02 06:02:12 +0530

    day2

---
+++ Tutorial/Day2/2.variable.js
@@ -0,0 +1,93 @@
+// 4 type to declare a variable
+// naming Convention
+// what to use
+
+// 1 way to declare a variable
+
+a = 10;
+console.log(a);
+a;
+
+// var ,let ,const
+
+// 2 var 
+
+var b;
+b = 20;
+b = 30;
+b;
+
+var c = 30;
+c;
+
+// 3 let 
+
+let d = 10,
+    e = 90,
+    f = 12;
+
+d = 13;
+d;
+e;
+f;
+
+
+// 4 const (use when you this the is not change)
+
+const g = 10;
+g;
+
+
+const pi = 3.14;
+
+
+// namingConvention
+
+// variable must be start with char(letter) or $ or _
+// variable not be start with digit
+// variable not start with inbuilt keyword(let ,const ,if ,console)
+
+// let 2a=10
+// let if=10
+
+let ab = 10;
+let $ = 10;
+// let a = 10;
+
+
+let num = 10;
+// camelCase
+
+let numTwo = 20;
+numTwo;
+
+// snake_case
+let num_two = 10;
+
+// PascalCase
+
+let NumTwo = 10;
+
+// variable are case sensitive
+
+let m = 80;
+let M = 10;
+M;
+m;
+
+
+let numOne = 10;
+numone = 20;
+numone;
+numOne;
+
+
+// let ya const 
+
+const productOne = 20;
+const productTwo = 30;
+
+let total = productOne + productTwo;
+
+total;
+console.log(total);
