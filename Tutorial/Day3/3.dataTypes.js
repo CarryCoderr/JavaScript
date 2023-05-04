a581deb3f99b3e17c66f6fbeb72b8aa5a8d4be45
Carry Coder <carrycoderr@gmail.com>
2023-05-04 12:44:17 +0530

    day3

---
+++ Tutorial/Day3/3.dataTypes.js
@@ -0,0 +1,106 @@
+// Data Types
+// Primitive Data - Number,String,Boolean, BigInt,Undefined ,null,(Symbol)
+// Non primitive - Array , Object
+
+// Number
+let numOne = 10;
+numOne;
+
+console.log(typeof 10);
+
+console.log(typeof numOne);
+
+let numTwo = 10.5;
+
+console.log(numTwo);
+console.log(typeof numTwo);
+console.log(typeof (numTwo));
+
+// String
+
+let strOne = "Hello World ";//using double ""
+let strTwo = 'Hello World';// using single ''
+
+strOne;
+strTwo;
+
+console.log(typeof strOne);
+
+// Boolean (true(1),false(0))
+
+let valueTrue = true;
+let valueFalse = false;
+
+valueTrue;
+
+valueFalse;
+
+console.log(typeof valueTrue);
+
+console.log(typeof valueFalse);
+
+
+// BigInt
+let largeNumber = 123214324353545454646454545n;
+
+let largeNumberTwo = 48845454545454545848844848484n;
+
+largeNumber;
+
+largeNumberTwo;
+
+console.log(typeof largeNumber);
+
+
+console.log(typeof largeNumberTwo);
+
+
+let bigIntUsingKeyW = BigInt(54548498448);
+
+console.log(typeof bigIntUsingKeyW);
+
+// undefined
+
+
+let undefinedValue;
+undefinedValue;
+
+console.log(typeof undefinedValue);
+
+
+// null
+
+
+let nullVar = null;
+
+console.log(nullVar);
+
+console.log(typeof nullVar);
+
+
+// redeclare of variable
+
+// let ,var,const
+
+// in var case
+var varNum = 10;
+var varNum = "new Value";
+
+varNum;
+
+var varNum = 20;
+varNum;
+
+// in let case
+
+let letNum = 10;
+// let letNum = "new Value";
+
+// same as in const
+
+
+
+
+
+
+
