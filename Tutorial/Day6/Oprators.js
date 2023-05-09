29150d815f0475abdb51a03c93cff994212efd55
Carry Coder <carrycoderr@gmail.com>
2023-05-09 13:26:56 +0530

    day6

---
+++ Tutorial/Day6/Oprators.js
@@ -0,0 +1,119 @@
+// ---------Operators Type-----------
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+// Type Operators
+
+
+// Arithmetic Operators
+
+// +
+
+
+let numOne = 10;
+let numTwo = 20;
+
+console.log(numOne + numTwo);
+
+
+// -
+
+console.log(numOne - numTwo);
+
+
+// *
+
+
+console.log(numOne * numTwo);
+
+
+// '/'
+
+
+console.log(numOne / numTwo);
+
+console.log(numTwo / numOne);
+
+
+// '%'
+
+console.log(numOne % numTwo);
+
+console.log(5 / 2);
+
+console.log(5 % 2);
+
+// '**' exponentiation (Ecma-2016)
+
+
+console.log(numOne ** 3);
+
+numOne * numOne * numOne;
+
+console.log(4 ** 3);
+
+
+// increment ++
+
+numOne;
+
+
+numOne++;
+
+numOne;
+
+
+// decrement --
+
+numOne;
+
+numOne--;
+
+numOne;
+
+
+// js concept in Arithmetic Operation
+
+// +
+let numThree = 30;
+
+
+let str = "20";
+
+let a = numThree + str;
+
+a;
+
+
+console.log(typeof a);
+
+
+// -
+let b = numThree - str;
+
+b;
+
+console.log(typeof b);
+
+// *
+
+console.log(numThree * str);
+
+
+// type Operators
+
+// 1.typeof
+// 2.instanceof
+
+console.log(typeof 10);
+
+console.log(typeof "10");
+
+console.log(typeof true);
+
+console.log(typeof 12n)
+
