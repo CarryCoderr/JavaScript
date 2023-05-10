a896345366de69d35387342843c9811d1f75d61a
Carry Coder <carrycoderr@gmail.com>
2023-05-10 15:10:33 +0530

    Day7

---
+++ Tutorial/Day7/7.Assignment.js
@@ -0,0 +1,78 @@
+// Assignment Operators
+
+// =
+let numOne = 10;
+
+let numTwo = 20;
+
+let a = 10;
+let b = a;
+
+b;
+
+a = 5;
+a;
+
+// const a = 10;
+// const b = a;
+
+// b;
+
+// a = 5;
+// a
+
+numOne;
+
+numTwo;
+
+// += (Addition Assignment Operator)
+
+
+console.log(numOne += numTwo);
+
+// -= (Subtraction Assignment Operator)
+numOne;
+
+numTwo;
+
+
+console.log(numOne -= numTwo);
+
+numOne;
+
+// *= (Multiplication Assignment Operator)
+
+numTwo;
+
+console.log(numOne *= numTwo);
+
+// /= (Division Assignment Operator) (self practice)
+
+// %= (Modulus Assignment Operator)
+
+numOne;
+
+numTwo;
+
+console.log(numTwo %= numOne);
+
+
+// **= (exponentiation Assignment Operator)
+
+
+console.log(2 ** 2);
+
+numOne;
+numTwo;
+console.log(numOne **= 2);
+
+numOne;
+
+let numThree = 5;
+
+
+console.log(numThree **= 3);
+
+numThree
+
+
