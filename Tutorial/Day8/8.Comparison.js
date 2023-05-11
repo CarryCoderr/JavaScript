ec66ed28aa02caee2816f9db9b85c66983a5cb9d
Carry Coder <carrycoderr@gmail.com>
2023-05-11 18:18:07 +0530

    ComparisonOp

---
+++ Tutorial/Day8/8.Comparison.js
@@ -0,0 +1,89 @@
+// Comparison Operators
+
+// == (equal to)
+
+
+let numOne = 10;
+
+let numTwo = 20;
+
+let numThree = 10;
+
+let str = "10";
+
+console.log(typeof str);
+
+console.log(typeof numOne);
+
+console.log(numOne == str);
+
+
+// === equal value and equal type
+
+console.log(numOne === str);
+
+
+// != (not equal to)
+
+
+console.log(numOne != numTwo);
+
+console.log(numOne != str);
+
+
+// !== (not equal value or not equal type)
+
+numOne;
+
+str;
+
+console.log(numOne !== str);
+
+console.log(numOne !== numThree);
+
+console.log("hi" !== "hi");
+
+
+// > (greater than)
+
+
+
+console.log(numOne > numThree);
+
+console.log(numTwo > numOne);
+
+
+//  < (less than)
+
+
+console.log(numOne < numTwo);
+
+console.log(numOne < numThree);
+
+
+// <= (less than or equal to)
+
+
+console.log(numOne <= numThree);
+
+
+console.log(numOne <= numTwo);
+
+
+console.log(numTwo <= numOne);
+
+
+// >= (greater than or equal to)
+
+console.log(numOne >= numThree);
+
+console.log(numTwo >= numOne);
+
+
+// Comparison with string
+
+console.log('2' > '12');
+
+
+
+
