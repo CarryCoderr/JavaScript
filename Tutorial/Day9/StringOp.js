5573b8f861daf9285f341a4e01157af6715abc6c
Carry Coder <carrycoderr@gmail.com>
2023-05-13 12:59:27 +0530

    string operator

---
+++ Tutorial/Day9/StringOp.js
@@ -0,0 +1,75 @@
+// String Operators
+
+// concatenation 
+
+// concatenation assignment
+
+
+let strOne = "Carry";
+
+let strTwo = "Coder";
+
+
+// concatenation
+
+console.log(strOne + strTwo);
+
+console.log(strOne + " " + strTwo);
+
+strOne;
+
+strTwo;
+
+
+// concatenation assignment
+
+console.log(strOne += strTwo);
+
+strOne;
+
+
+console.log(strOne += strOne);
+
+console.log(strOne -= strOne);
+
+
+// concatenation assignment with number type String
+
+
+let strNumOne = "10";
+
+let strNumTwo = "10";
+
+
+
+console.log(strNumOne + strNumTwo);
+
+console.log(strNumOne *= strNumTwo);
+
+console.log(typeof strNumOne);
+
+
+// comparison with string 
+
+console.log('2' > '12');
+
+// 2  ==> 2
+// 12 ==> 1
+
+
+console.log('2' > '1');
+
+
+console.log("10" > "10");
+
+console.log("11" > "12");
+
+// 11 ==> 1==>1
+// 12 ==> 1==>2
+
+console.log("12" > "12asd");
+
+
+
+
+
