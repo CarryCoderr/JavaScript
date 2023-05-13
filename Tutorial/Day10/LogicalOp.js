75df979c4d14e5571e843581506050590de11b6b
Carry Coder <carrycoderr@gmail.com>
2023-05-13 13:02:17 +0530

    logical operator

---
+++ Tutorial/Day10/LogicalOp.js
@@ -0,0 +1,45 @@
+// Logical Operator
+
+// && (and) (if both Operation are True then return true and vice-versa)
+
+
+let numOne = 10;
+
+let numTwo = 20;
+
+let numThree = 10;
+
+
+console.log(numOne < 10 && numTwo < 20);
+
+console.log(numOne <= 10 && numTwo <= 20);
+
+
+// || (or) if one of the condition is true then return true else false
+
+console.log(numOne < 10 || numTwo < 20);
+
+console.log(numOne <= 10 || numTwo <= 20);
+
+// !  (not)
+
+numOne;
+
+numTwo;
+
+numThree;
+
+console.log(!(numOne == numThree));
+
+console.log(!(numOne == numTwo));
+
+
+// apply with number and String
+
+let str = "10";
+
+console.log(numOne <= 10 && str <= 10)
+
+
+
+
