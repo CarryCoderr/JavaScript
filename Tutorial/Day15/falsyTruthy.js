7774971b74687d09fcaee20625734bf6b3ed5a75
Carry Coder <carrycoderr@gmail.com>
2023-05-26 22:32:25 +0530

    falsy and Truthy

---
+++ Tutorial/Day15/falsyTruthy.js
@@ -0,0 +1,53 @@
+// falsy and Truthy values
+
+
+// falsy
+
+// null
+
+let nullValue = null;
+
+console.log(Boolean(nullValue));
+
+// undefined
+
+console.log(Boolean(undefined));
+
+
+// false
+
+console.log(Boolean(false));
+
+// NaN
+
+console.log(Boolean(NaN));
+
+// 0,-0,0.0,0n
+
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(0n));
+
+// "",'',``
+
+console.log(Boolean(""));
+console.log(Boolean(''));
+console.log(Boolean(``));
+
+// document.all
+
+// console.log(Boolean(document.all))
+
+
+// Truthy values 
+
+
+console.log(Boolean(10));
+
+console.log(Boolean('1'));
+
+console.log(Boolean({}));
+
+console.log(Boolean([]));
+
+
